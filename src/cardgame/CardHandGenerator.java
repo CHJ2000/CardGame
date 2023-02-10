e0187b1e2b938602eceef6eb75ad4541a44bb451
Carter Jeffery <jeffcart@sheridancollege.ca>
2023-02-10 13:16:46 -0500

    First commit for cardgenerator

---
+++ src/cardgame/CardHandGenerator.java
@@ -0,0 +1,29 @@
+package cardgame;
+
+import java.util.Random;
+
+public class CardHandGenerator {
+
+    /**
+     * 
+     * @param numCards
+     */
+    public static Card[] generateHand(int numCards) {
+               //create an array to hold 7 cards
+        Card[] hand = new Card[numCards];
+        
+        // use Random class to generate random numbers
+        Random random = new Random();
+        
+        for (int i = 0; i < hand.length; i++){
+            int numValues = Card.Value.values().length;
+            Card.Value value = Card.Value.values()[random.nextInt(numValues)];
+            
+            int numSuits = Card.Suit.values().length;
+            Card.Suit suit = Card.Suit.values()[random.nextInt(numSuits)];
+            Card card = new Card(value, suit);
+            hand[i] = card;
+    }
+return hand;
+}
+    }
