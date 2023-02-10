742e48a5c6a5d890286f859cbf53d4a3fc3c6d2d
Carter Jeffery <jeffcart@sheridancollege.ca>
2023-02-10 12:52:45 -0500

    added enums

--- src/cardgame/Card.java
+++ src/cardgame/Card.java
@@ -1,51 +1,14 @@
 package cardgame;
 
 public class Card {
-   private String suit;
-   private int value;
-   
-   public static final String[] SUITS = {"Hearts", "Diamonds", "Spades", "Clubs"};
 
-    // created a two argument constructor
-    public Card(int value, String suit){
-        this.value = value;
-        this.suit = suit;
-    }
-    
-    public int getValue() {
-        return this.value;
-    }
-
-    /**
-     * 
-     * @param value
-     */
-    public void setValue(int value) {
-        this.value = value;
-    }
-
-    public String getSuit() {
-        return this.suit;
-    }
-
-    /**
-     * 
-     * @param suit
-     */
-    public void setSuit(String suit) {
-        this.suit = suit;
-    }
-
-
-
-    public enum Suit {
+     public enum Suit {
         HEARTS,
         CLUBS,
         SPADES,
         DIAMONDS
     }
-
-
+  
     public enum Value {
         ACE,
         TWO,
@@ -60,6 +23,40 @@ public class Card {
         JACK,
         QUEEN,
         KING
+    }
+    
+    private Value value;
+    private Suit suit;
+
+    
+    // created a two argument constructor
+    public Card(Value value, Suit suit){
+        this.value = value;
+        this.suit = suit;
+    }
+    
+    public Value getValue() {
+        return this.value;
+    }
+
+    /**
+     * 
+     * @param value
+     */
+    public void setValue(Value value) {
+        this.value = value;
+    }
+
+    public Suit getSuit() {
+        return this.suit;
+    }
+
+    /**
+     * 
+     * @param suit
+     */
+    public void setSuit(Suit suit) {
+        this.suit = suit;
     }
 
 }
